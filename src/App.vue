4ee2c5db45d11c07dd3e29e10c84cd3730946aec
AidanSidebottom <aidans42@educbe.ca>
2022-12-28 16:34:07 -0700

    started working on adding assets

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
   import{ref, computed} from 'vue'
 
+  let currentMove = true;
+  
+  const food = {name:"food",color:"red",image:"./assets/apple.png"}
+  const head ={name:"head",color:"blue",image:".assets/apple.png"}
+  const body = {name:"body",color:"blue",image:"b"}
+  const tail ={name:"tail",color:"blue",image:"t"}  
+
   const area = ref([
   
   ['','','','','','','','','',''],
   ['','','','','','','','','',''],
   ['','','','','','','','','',''],
   ['','','','','','','','','',''],
-  ['','','','','','','','','',''],
+  ['','','',head,'','',food,'','',''],
   ['','','','','','','','','',''],
   ['','','','','','','','','',''],
   ['','','','','','','','','',''],
@@ -24,9 +31,23 @@ if((x+y)%2==0){
     return 'black';
   }
 }
+
+let rightPeice = () =>{
+area[x][y].value.name==='food'
+}
+
+
+let start = () =>{
+  alert("the game has started")
+}
+
+
 </script>
 
 <template>
+  <div class="title">
+    Snake
+  </div>
  <main class="area">
       <div
         
@@ -38,12 +59,19 @@ if((x+y)%2==0){
           :key="y"
           
           v-bind:class="tiles(x,y)"
-          
-
         >
+        <div>
+          <img v-bind:src="area[x][y].image">
         </div>
+            
+          
+        </div>
+        
       </div>
     </main>
+    <button v-on:click=start()>
+      start
+    </button>
 </template>
 
 <style scoped>
@@ -57,8 +85,8 @@ if((x+y)%2==0){
   vertical-align:middle;
   
   
-  height:20px;
-  width:20px;
+  height:40px;
+  width:40px;
   background-color: rgb(37, 202, 111);
   padding:10px;
 }
@@ -67,9 +95,18 @@ if((x+y)%2==0){
   vertical-align:middle;
   
   
-  height:20px;
-  width:20px;
+  height:40px;
+  width:40px;
   background-color: rgb(12, 117, 61);
   padding:10px;
+  
+}
+.title{
+  font-size: 25px;
+  padding-bottom: 15px;
+  
+}
+.apple{
+  src: "./assets/apple.png";
 }
 </style>
